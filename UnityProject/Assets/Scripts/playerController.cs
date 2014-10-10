8012120ea68709d00b54d7852400ee00db87ae31
arvindku <arvindk.86@gmail.com>
2014-10-09 17:19:56 -0700

    - added playerController, got basic version of movement smoothened out - added assets in scene for testing

---
+++ UnityProject/Assets/Scripts/playerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	public float playerSpeed = 10.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate() {
+	
+		
+		float verticalSpeed = Input.GetAxisRaw("Vertical");
+		float horizontalSpeed = Input.GetAxisRaw("Horizontal");
+		
+		Vector3 velocity = new Vector3(horizontalSpeed, 0.0f , verticalSpeed);
+
+		rigidbody.AddForce(velocity*playerSpeed);
+		
+		bool isGettingInput = false;
+		
+		//speed cap
+		float currentSpeed = rigidbody.velocity.magnitude;
+		if (currentSpeed > playerSpeed)
+		{
+			velocity = rigidbody.velocity.normalized;
+			rigidbody.velocity = velocity*playerSpeed;
+		}
+		/*
+		if (velocity.magnitude < 0.999)
+		{
+			rigidbody.velocity = Vector3.zero;
+		}
+		*/
+		Debug.Log(Input.GetAxisRaw("Vertical"));
+		
+	}
+	
+	
+}
